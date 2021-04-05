b993b08df6b4b7e4172376e6de5d86ea1d33a365
meruyert.aitkaliyeva <34997107+meruyertaitkaliyeva@users.noreply.github.com>
2021-04-05 10:06:55 +0400

    Корректирует мобильное меню без js

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,8 @@
+let headerMain = document.querySelector(".header");
 let navMain = document.querySelector(".header__navigation");
 let navToggle = document.querySelector(".header__toggle");
 
-navMain.classList.remove("header__navigation--nojs");
+headerMain.classList.remove("header--nojs");
 
 navToggle.addEventListener("click", function() {
   if (navMain.classList.contains("header__navigation--closed")) {
