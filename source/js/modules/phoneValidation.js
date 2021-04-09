64a34511313a74661472c265b4bae9cd27e1e959
meruyert.aitkaliyeva <34997107+meruyertaitkaliyeva@users.noreply.github.com>
2021-04-09 17:33:35 +0400

    Вносит правки по багам

--- source/js/modules/phoneValidation.js
+++ source/js/modules/phoneValidation.js
@@ -2,14 +2,19 @@
 
 (function () {
   const form = document.querySelector('form');
+  const error = form.querySelector('.intro__error');
   const phone = form.querySelector('#tel');
   const submit = form.querySelector('button');
 
   submit.addEventListener('click', function (evt) {
     if (phone.value.length < 10) {
       evt.preventDefault();
+      error.classList.remove('hide');
+      error.classList.add('show');
       return false;
     } else {
+      error.classList.remove('show');
+      error.classList.add('hide');
       return true;
     }
   })
